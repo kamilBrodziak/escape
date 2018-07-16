a13324c8dc9eeec30959c5dd9cf7e4df4d50a1ef
Kamil Brodziak <kamil98brodziak@gmail.com>
2018-07-16 23:44:16 +0200

    Added some funcionalities related with player and mob class. Added multithreading!

--- map.py
+++ map.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import threading
+import time
 
 
 def cls():  # clearing screen in terminal
@@ -13,17 +15,18 @@ class Map:
     def __init__(self, radius, filename):
         self.radius = radius
         with open(filename) as mapfile:
-            self.map = mapfile.read().splitlines()
+            self.ascii_map = mapfile.read().splitlines()
 
     def change_radius(new_radius):
         self.radius = new_radius
 
     def map_load(self, posx, posy):
         radiusx_start = posx - 2 * self.radius if posx - 2 * self.radius >= 0 else 0
-        radiusx_end = posx + 2 * self.radius if posx + 2 * self.radius <= len(self.map[0]) else len(self.map[0])
+        radiusx_end = posx + 2 * self.radius if posx + 2 * self.radius <= len(self.ascii_map[0]) else \
+            len(self.ascii_map[0])
         radiusy_start = posy - self.radius if posy - self.radius >= 0 else 0
-        radiusy_end = posy + self.radius if posy + self.radius <= len(self.map) else len(self.map)
-        map_copy = self.map[:]
+        radiusy_end = posy + self.radius if posy + self.radius <= len(self.ascii_map) else len(self.ascii_map)
+        map_copy = self.ascii_map[:]
         map_copy[posy] = list(map_copy[posy])
         map_copy[posy][posx] = "@"
         map_copy[posy] = "".join(map_copy[posy])
@@ -43,10 +46,46 @@ class Player:
         self.posy = 2
         self.key = False
         self.thirst = 100
+        self.hunger = 100
+        self.attack = 10
+        self.sword = 0
+        self.boots = 0
+        self.chest = 0
+        self.pants = 0
+        self.helmet = 0
+        self.defence = self.boots + self.pants + self.chest + self.helmet
+        self.life = 100
+        self.inventory = {}
 
     def change_pos(self, newposx, newposy):
         self.posx = newposx
         self.posy = newposy
+
+    def attack_mob(self, mob):
+        mob.health -= self.attack
+        if self.sword == mob.prone:
+            mob.health -= self.attack
+
+    def inventory_add(self):
+        pass
+
+    def inventory_equip(self):
+        pass
+
+
+class Mob:
+    def __init__(self, name, attack, defence, life, have_key, posx, posy):
+        self.name = name
+        self.posx = 2
+        self.posy = 2
+        self.attack = attack
+        self.defence = defence
+        self.life = life
+        self.prone = prone
+        self.have_key = have_key
+
+    def attack_player(self, player):
+        player.health -= self.attack * (1 - player.defence)
 
 
 def getChar(bits):  # get tke pressed key from user
@@ -67,25 +106,19 @@ def getChar(bits):  # get tke pressed key from user
         return answer
 
 
-def arrows_move(posx, posy, char):  # moving in menu
+def arrows_move(posx, posy, char, game):  # moving in menu
     arrows_ud = {"[A": -1, "[B": 1, "[C": 0, "[D": 0}  # up down
     arrows_lr = {"[A": 0, "[B": 0, "[C": 1, "[D": -1}  # left right
-    if posx + arrows_lr[char] < 0 or posx + arrows_lr[char] > len(game.map[0]) - 1 or \
-        posy + arrows_ud[char] < 0 or posy + arrows_ud[char] > len(game.map) - 1 or \
-            game.map[posy + arrows_ud[char]][posx + arrows_lr[char]] == "#":
+    if posx + arrows_lr[char] < 0 or posx + arrows_lr[char] > len(game.ascii_map[0]) - 1 or \
+        posy + arrows_ud[char] < 0 or posy + arrows_ud[char] > len(game.ascii_map) - 1 or \
+            game.ascii_map[posy + arrows_ud[char]][posx + arrows_lr[char]] == "#":
         return posx, posy
     return posx + arrows_lr[char], posy + arrows_ud[char]
 
 
-game = Map(120, "map1.txt")
-posx = 2
-posy = 2
-gamer = Player("kamil", posx, posy)
-game.map_load(posx, posy)
-
-
-def print_game_window(game):
+def print_game_window(game, gamer):
     cls()
+    print(gamer.thirst)
     print("\n"*5)
     length_window_game = len(game.map_show.split("\n")[0]) - 12
     print(10 * " ",  length_window_game * "-")
@@ -93,11 +126,47 @@ def print_game_window(game):
     print(10 * " ", length_window_game * "-")
 
 
-while True:
-    print_game_window(game)
-    char = getChar(1)
-    if(char == "\x1b"):
-        char = getChar(2)
-        posx, posy = arrows_move(posx, posy, char)
-        gamer.change_pos(posx, posy)
-        game.map_load(posx, posy)
+def test(gamer, run_event):
+    while run_event.is_set():
+        time.sleep(1)
+        gamer.thirst -= 1
+    return
+
+
+def gamestart(gamer, game, posx, posy, run_event):
+    while run_event.is_set():
+        print_game_window(game, gamer)
+        char = getChar(1)
+        if char == "\x1b":
+            char = getChar(2)
+            posx, posy = arrows_move(posx, posy, char, game)
+            gamer.change_pos(posx, posy)
+            game.map_load(posx, posy)
+        elif char == "i":
+            pass
+    return
+
+
+def main():
+    game = Map(5, "map1.txt")
+    posx = 2
+    posy = 2
+    gamer = Player("kamil", posx, posy)
+    game.map_load(posx, posy)
+    run_event = threading.Event()
+    run_event.set()
+    t2 = threading.Thread(target=test, args=(gamer, run_event, ))
+    t1 = threading.Thread(target=gamestart, args=(gamer, game, posx, posy, run_event))
+    t1.start()
+    t2.start()
+    try:
+        while 1:
+            time.sleep(.1)
+    except KeyboardInterrupt:
+        run_event.clear()
+        t1.join()
+        t2.join()
+        return
+
+
+main()
